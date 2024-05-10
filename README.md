5d6d4f10b3f6a6cb3bee90cf4990b0ae35e9578a
Dante Mogrim <66732473+dantemogrim@users.noreply.github.com>
2024-05-10 13:26:58 +0200

    Add .dotenv like solution for Firebase credentials

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 - Node.js *(see exact version in `./.nvmrc`)*
 - npm
 
+🔥 This project requires you setting up your very own Firebase project. Once you've done that, you'll need to configure the following:
+- A Firestore Database *(look into 'Storage')*.
+  - Start a collection named `reviews` containing the fields `game` *(string)* and `text` *(string)*.
+- Add a web app of your project.
+  - Copy your given SDK credentials and paste them in accordingly into `src/env-example.ts`. Once finished, rename `env-example.ts` into `env.ts`.
+
 Create a new shell session and `cd` into root directory.
 
 ```shell
@@ -18,4 +24,4 @@ $ npm run start
 ```
 
 In the output you should find a url from where the project is running.
-Visit the given url from your browser. Now you should be able to use the project as intended.
+Visit the given url from your browser. Now you should be able to interact with the project as intended.
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { env } from '../env';
 import { routes } from './app.routes';
-import { GameService } from './services/game.service';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
+const firebaseConfig = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    GameService,
-    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+    ]),
   ],
 };
